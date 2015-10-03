966fb078c887f327cf95463d4fc826669e1714fc
Emily Lordahl <contact@emilylordahl.com>
2015-10-03 19:03:00 -0400

    fixing white space

--- server.js
+++ server.js
@@ -14,30 +14,30 @@ app.use(express.static('./public'));
 
 // Get Number of Twitter Followers Based on Twitter Handle
 app.get('/search', function(req, res) {
-	// Get and store search query from front-end input
-	var twitterHandle = (req.query.handle);
+  // Get and store search query from front-end input
+  var twitterHandle = (req.query.handle);
   console.log(twitterHandle);
-	request({
-		uri: 'https://twitter.com/' + twitterHandle,
-		json: true
-	}, function (error, response, html) {
-		if (!error && response.statusCode == 200) {
-			// Load HTML from Cheerio Scraper
-			var $ = cheerio.load(html);
-			// Store Data from Targeted DOM Elements
-			var followerHtml = $('li.ProfileNav-item.ProfileNav-item--followers').children();
+  request({
+    uri: 'https://twitter.com/' + twitterHandle,
+    json: true
+  }, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      // Load HTML from Cheerio Scraper
+      var $ = cheerio.load(html);
+      // Store Data from Targeted DOM Elements
+      var followerHtml = $('li.ProfileNav-item.ProfileNav-item--followers').children();
       console.log("FOLLOWER?", followerHtml);
       if (followerHtml.length === 0) {
         res.json({ status: 404, message: "Not a valid username."});
       } else {
         // Parse and store follower information
-  			var followers = followerHtml[0].attribs;
+        var followers = followerHtml[0].attribs;
         console.log(followers);
-  			// Send back the follower count
-  			res.send(followers);
+        // Send back the follower count
+        res.send(followers);
       }
-		}
-	});
+    }
+  });
 });
 
 app.listen(process.env.PORT || 8080, function() {
